949e5bc09c60bd91ad0af54bdbd442f15caa2de1
D4MAGE_WIZ4RD <axel.d4mage@gmail.com>
2024-12-30 18:35:44 -0300

    Update files

--- fast.py
+++ fast.py
@@ -9,12 +9,12 @@ class MENU:
             "2": self.theme_maker,
             "3": self.reset_to_default,
             "4": self.select_image,
-            "5": self.salir
-    }
+            "5": self.salir,
+        }
 
     def menu(self):
         while True:
-            number_option = input("#:")
+            number_option = input("#: ")
             action = self.options.get(number_option)
             if action:
                 action()
@@ -22,61 +22,64 @@ class MENU:
                 print("Option not valid")
 
     def select_theme(self):
-        print("1. Default")
-        theme = input("#:")
-        with open(theme) as file:
-            file = json.load(file)
+        
+        content = os.listdir(os.getcwd()+"/Faster/default_themes")
+        for index, item in enumerate(content, 1):
+            print(f"{index}.{item.replace(".jsonc", "")}")
+        theme = input("\n#: ")
+        os.system('fastfetch -c /home/d4mag3/Documentos/Programacion/Back/Faster/default_themes/all.jsonc')
+        try:
+            with open(theme) as file:
+                data = json.load(file)
+                print("Theme loaded:", data)
 
+                
+        except FileNotFoundError:
+            print("File not found.")
+        except json.JSONDecodeError:
+            print("Invalid JSON format.")
 
     def theme_maker(self):
         print("1. Change color")
-        theme = input("#:")
+        theme = input("#: ")
 
     def reset_to_default(self):
         print("Are you sure you want to reset to default? (y/n)")
-        theme = input("#:")
+        theme = input("#:S ")
 
     def select_image(self):
         print("Select image")
-        theme = input("#:")
+        theme = input("#: ")
+
+    def load_json(self) -> dict:
+        try:
+            with open('/home/d4mag3/Documentos/Programacion/Back/Faster/Banners/banner.json') as f:
+                return json.load(f)["logo"]["string"]
         
-    def load_json(sefl) -> dict:
-        with open('/home/d4mag3/Documentos/Programacion/Back/Faster/Json/banner.json') as f:
-            return json.load(f)["logo"]["string"]
-        
+        except FileNotFoundError:
+            print("File not found.")
+
+        except json.JSONDecodeError:
+            print("Invalid JSON format.")
+        return {}
+
     def salir(self):
+        print("Exiting program.")
         exit()
 
     def __str__(self):
-        pass
-        
-class Faster(MENU):
+        return "MENU Class"
 
+
+class Faster(MENU):
     def __init__(self):
         super().__init__()
-        os.system('clear')
-        super().load_json()
+        #os.system('clear')
 
-    def menu(self):
-        super().menu()
-
-    def select_theme(self):
-        super().select_theme()
-        
-    def theme_maker():
-        super().theme_maker()
-        
-    def reset_to_default(self):
-        super().reset_to_default()
-        
-    def select_image(self):
-        super().select_image()
-    
     def __str__(self):
-        pass
+        return "Faster Class"
 
-fast = Faster()
-fast.menu()
 
 if __name__ == "__main__":
-    main()
+    fast = Faster()
+    fast.menu()
