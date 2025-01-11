248223d55371baa9d4f9fe52f37b9d6edd9998eb
D4MAGE_WIZ4RD <axel.d4mage@gmail.com>
2025-01-11 17:15:57 -0300

    Update files

--- faster_wizard.py
+++ faster_wizard.py
@@ -2,7 +2,9 @@
 import os
 import json
 import time 
-from colorama import Fore
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 directory = os.getcwd()
 class MENU:
     def __init__(self):
@@ -23,45 +25,62 @@ class MENU:
         os.system(f'echo "{fast.load_json()}" | lolcat')
         while True:
             fast.load_json()
-            print("Wellcome to the Faster Wizard, please select an option from below.\n1 - Select from default themes\n2 - Theme maker\n3 - Reset to default\n4 - Select logo\n5 - Show my configuration\n6 - Exit")
-            number_option = input("#: ")
+            print(Fore.CYAN + "Wellcome to the Faster Wizard, please select an option from below.")
+            print(Fore.GREEN + "1 - Select from default themes" + Fore.RESET + "   " + Fore.YELLOW + "2 - Theme maker")
+            print(Fore.RED + "3 - Reset to default" + Fore.RESET + "             " + Fore.MAGENTA + "4 - Select logo")
+            print(Fore.BLUE + "5 - Show my configuration" + Fore.RESET + "        " + Fore.WHITE + "6 - Exit")
+            number_option = input(Fore.CYAN + "#: " + Fore.RESET)
             action = self.options.get(number_option)
             if action:
                 action()
             else:
-                print("Option not valid")
-
-#APPLY THEMES  --------------
+                print(Fore.RED + "Invalid option" + Fore.RESET)
 
 #SELECT THEME --------------
 
     def select_theme(self):
         while True:
             os.system('clear')
-            content = os.listdir(os.getcwd()+"/default_themes") #No mover ruta
-            print("Select a theme for preview\nPlease use fullscreen for a better look\n")
-            print("0 - Exit")
+            content = os.listdir(os.getcwd()+"/default_themes")
+            print(Fore.RED + "0. Exit")
             for index, item in enumerate(content, 1):
-                print(f"{index}.{item.replace(".jsonc", "")}")
+                print(Fore.YELLOW + f"{index}.{item.replace('.jsonc', '')}")
+            print(Fore.BLUE + "Select a theme for preview\nPlease use fullscreen for a better look\n" + Fore.RESET)
 
     #VISUALIZE THEME --------------
             data = {str(idx): i for idx, i in enumerate(content, 1)}
             theme = input("\n#: ")
 
-            if theme == "0":
+            if theme == "":
+                print(Fore.RED + "Invalid option" + Fore.RESET)
+                time.sleep(0.5)
+                self.select_theme()
+            elif theme == "0":
                 fast.menu()
+            elif theme not in data:
+                print(Fore.RED + "Invalid option" + Fore.RESET)
+                time.sleep(0.5)
+                self.select_theme()
+                
+            
             directory = os.getcwd()
-            
-            os.system(f'fastfetch -c {directory}/default_themes/{data[theme]}') #No mover ruta
-            
+            os.system(f'fastfetch -c {directory}/default_themes/{data[theme]}')
+
             print("¿Apply Theme?")
             apply_theme = input("1 - Yes\n2 - No\n#: ")
             
             if apply_theme == "1":
-                print("Theme applied.")
-                time.sleep(1) #########################
+                print("Applying theme...")
+                time.sleep(1)
                 os.system('clear')
-                #os.system(f'nano ~/.config/fastfetch/config.jsonc') #No mover ruta solo comando, aplicar tema
+                id = data[theme]
+                os.system(f'rm ~/.config/fastfetch/config.jsonc')
+                time.sleep(0.5)
+                os.system(f'cp default_themes/{id} ~/.config/fastfetch/')
+                time.sleep(0.5)
+                os.system(f'mv ~/.config/fastfetch/{id} ~/.config/fastfetch/config.jsonc')
+                print("Theme applied! Check your terminal.")
+                time.sleep(1.5)
                 fast.menu()
                 try:
                     with open(theme) as file:
@@ -89,17 +108,16 @@ class MENU:
 
     def reset_to_default(self):
         os.system('clear')
-        print("Are you sure you want to reset to default? (y/n).")
-        theme = input("#: ")
+        print(Fore.RED + "Are you sure you want to reset to default? (y/n)." + Fore.RESET)
+        theme = input(Fore.CYAN + "#: " + Fore.RESET)
         if theme == "y":
-            print("Reseting to default...")
-            #TODO
+            print(Fore.YELLOW + "Reseting to default..." + Fore.RESET)
+            os.system('fastfetch --gen-config-force')
             time.sleep(1)
             os.system('fastfetch')
-            print("This is your current fastfetch configuration.")
-            
+            print(Fore.GREEN + "Reset completed." + Fore.RESET)
         else:
-            print("Returning to menu...")
+            print(Fore.YELLOW + "Returning to menu..." + Fore.RESET)
             time.sleep(1)
             os.system('clear')
             fast.menu()
@@ -108,20 +126,27 @@ class MENU:
 
     def select_logo(self):
         os.system('clear')
-        print("1 - Logo list\n2 - Show all logos in terminal\n3 - Select path to custom image or ASCII.")
-        theme = input("#: ")
+        print(Fore.GREEN + "1 - Logo list" + Fore.RESET + "\n" + Fore.YELLOW + "2 - Show all logos in terminal" + Fore.RESET + "\n" + Fore.CYAN + "3 - Select path to custom image or ASCII." + Fore.RESET)
+        theme = input(Fore.CYAN + "#: " + Fore.RESET)
         if theme == "1":
             os.system('clear')
-            print("Logo list")
-            os.system('fastfetch --list-logos')
-            print("Select a logo number from the list.")
-            logo = input("#: ")
+            os.system('fastfetch --list-logos | lolcat')
+            print(Fore.BLUE + "Select a logo number from the list." + Fore.RESET)
+            logo = input(Fore.CYAN + "#: " + Fore.RESET)
         
         elif theme == "2":
             os.system('clear')
-            print("Select path to custom image or ASCII.\nIf the path is not correct, the default logo will be used.\nRemember that if you select an image you need a terminal that can support it.")
-            logo = input("#: ")
+            os.system('fastfetch --print-logos')
+            print(Fore.BLUE + "Press enter to return to the menu." + Fore.RESET)
+            input()
+            fast.menu()
+
+        elif theme == "3":
+            os.system('clear')
+            print(Fore.BLUE + "Select path to custom image or ASCII.\nIf the path incorrect, the default logo will be used.\nIf you select an image you need a terminal that can support it." + Fore.RESET)
+            logo = input(Fore.CYAN + "#: " + Fore.RESET)
             os.system(f'fastfetch --logo {logo}')
+
 #JSON LOAD BANNER --------------
 
     def load_json(self) -> dict:
@@ -137,16 +162,20 @@ class MENU:
         except json.JSONDecodeError:
             print("Invalid JSON format.")
         return {}
+    
+#EXIT --------------
 
     def salir(self):
-        print("Exiting program.")
+        print(Fore.RED + "Exiting..." + Fore.RESET)
         exit()
+
+#SHOW CURRENT CONFIGURATION --------------
 
     def show(self):
         os.system('clear')
         print("Your current configuration is:")
         os.system('fastfetch')
-        print("Press any key to return to the menu.")
+        print(Fore.BLUE+"Press enter to return to the menu.")
         input()
         fast.menu()
 
